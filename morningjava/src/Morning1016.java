7e31ca4262f78c0ded0615f44125a86314ac8db7
rhlanf47 <rhlanf47@naver.com>
2020-10-16 11:00:55 +0900

    first job

---
+++ morningjava/src/Morning1016.java
@@ -0,0 +1,21 @@
+public class Morning1016 {
+     public static void main(String[] args) {
+          System.out.println("Java Sample"); 
+
+          int[][] regAry = new int[5][5];
+          int cnt = 1;
+          //값을 지정하는 작업
+          for(int i=0; i<5; i++) {
+               for(int j=0; j<5; j++) {
+                    regAry[j][i] =cnt ++;
+               }
+          }
+          // 값을출력
+          for(int i=0; i<5; i++) {
+               for(int j=0; j<5; j++) {
+                    System.out.printf("%3d",regAry[i][j]);
+               }
+               System.out.println();
+          }
+     }
+}
---
+++ myhome.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType = "text/html; charset=utf-8" pageEncoding="utf-8"%>
+<!doctype html>
+<html>
+<head> 
+톰켓 구축띠 
+</head>
+<body>
+<h2> 톰켓 서비스 시작 </h2>
+<%
+String myname = "강민석";
+String today = (new java.util.Date()).toLocaleString();
+%>
+<strong><%= myname %></strong> 홈페이지에 잘왔다. <br>
+오늘은 : <%= today %> 입니당.
+	</p>
+</body>
+</html>
